538473e7d96e93e0cb8598a038a5cc9fa21122c1
Brady Snuggs <dronebug360@gmail.com>
2019-06-17 19:48:30 -0600

    initial commit

---
+++ socket_practice(EXERCISE)/public/client.js
@@ -0,0 +1,12 @@
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+
+
+
+
+
+
